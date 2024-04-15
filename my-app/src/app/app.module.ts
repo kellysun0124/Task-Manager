7ab63aef3f7d0ae6631717a2a5d06e18e0d326d3
Rosa Wessel <rosawessel@mwc-042210.dhcp.missouri.edu>
2024-04-15 14:24:58 -0500

    added body components and footer component

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { InputComponent } from './input/input.component';
+import { ViewComponent } from './view/view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    InputComponent,
+    ViewComponent
   ],
   imports: [
     BrowserModule,
