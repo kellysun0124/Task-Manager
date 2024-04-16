6cf83a3c197a8fbb21b3ff32422f7575fa74e6a3
Rosa Wessel <rosawessel@Rosas-MacBook-Pro.local>
2024-04-16 17:51:44 -0500

    added routing

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { TasksComponent } from './tasks/tasks.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'tasks', component: TasksComponent},
+
+  //redirects to home by default
+  { path: '**', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
