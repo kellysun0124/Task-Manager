16f9bba30aeca35e8e9abb32794f49acf6b80ce1
Rosa Wessel <rosawessel@Rosas-MacBook-Pro.local>
2024-04-16 17:48:07 -0500

    started the header, added register component and tasks component

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,29 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule} from '@angular/material/input'
+import { MatCardModule } from '@angular/material/card'
+import {MatButtonModule} from '@angular/material/button'
+import {MatToolbarModule} from '@angular/material/toolbar'
+import {MatExpansionModule} from '@angular/material/expansion'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { ViewComponent } from './view/view.component';
 import { LoginComponent } from './login/login.component';
-
-import { MatCardModule } from '@angular/material/card';
-import { FormsModule } from '@angular/forms';
-
+import { RegisterComponent } from './register/register.component';
+import { TasksComponent } from './tasks/tasks.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ViewComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    MatCardModule
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- my-app/src/app/register/register.component.spec.ts
+++ my-app/src/app/register/register.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ViewComponent } from './view.component';
+import { RegisterComponent } from './register.component';
 
-describe('ViewComponent', () => {
-  let component: ViewComponent;
-  let fixture: ComponentFixture<ViewComponent>;
+describe('RegisterComponent', () => {
+  let component: RegisterComponent;
+  let fixture: ComponentFixture<RegisterComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ViewComponent]
+      declarations: [RegisterComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(ViewComponent);
+    fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
