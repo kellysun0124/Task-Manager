9233a23a6e5fa072b49dd539d003396c1cd6279c
Rosa Wessel <rosawessel@mwc-042136.dhcp.missouri.edu>
2024-04-22 14:23:27 -0500

    added register stuff

--- my-app/src/app/register/register.component.ts
+++ my-app/src/app/register/register.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  username: string = '';
+  firstname: string = '';
+  lastname: string = '';
+  email: string = '';
+  phone: string = '';
+  password: string = '';
 
+  constructor(private http: HttpClient) {}
+
+  submitRegistration() {
+    const userData = {
+      userId: this.username,
+      firstName: this.firstname,
+      lastName: this.lastname,
+      email: this.email,
+      phone: this.phone,
+      password: this.password
+    };
+
+    this.http.post<any>('http://localhost:3001/register', userData).subscribe({
+      next: (response) => {
+        console.log('Registration successful:', response);
+      },
+      error: (error) => {
+        console.error('Registration failed:', error);
+      }
+    });
+  }
 }
