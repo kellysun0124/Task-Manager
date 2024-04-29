74897abf0b4e21fe4fda4b6b0578f7c08ae89184
Matthew Schutz <schutzmw3@gmail.com>
2024-04-29 14:31:53 -0500

    Updating User model

--- my-app/backend/Models/User.js
+++ my-app/backend/Models/User.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-    username: { type: String, required: true },
-    password: { type: String, required: true }
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  email: String,
+  firstName: String,
+  lastName: String,
+  phoneNumber: String
 });
 
 module.exports = mongoose.model('User', userSchema);
