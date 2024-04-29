1ceeff43c7c0a67fd185cf04370b2ad5ce917879
Matthew Schutz <schutzmw3@gmail.com>
2024-04-28 21:48:34 -0500

    Final CICD revisions

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,15 @@
-# Stage 1: Build Angular app
 FROM node:20 as builder
 
-# Set working directory
 WORKDIR /app
 
 RUN npm install -g pm2
 
-# Copy package.json and package-lock.json
 COPY my-app/package*.json ./
 
-# Install Angular CLI
 RUN npm install -g @angular/cli
 
-# Install dependencies
 RUN npm install
 
-# Copy Angular app source code
 COPY my-app .
 
 EXPOSE 4200 3000
--- my-app/backend/app.js
+++ my-app/backend/app.js
@@ -34,14 +34,14 @@ app.use((req, res, next) => {
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello World!!!!!');
+    res.send('Hello World!');
 });
 
 app.get('/users/:username', async (req, res) => {
     const username = req.params.username;
     try {
         const user = await User.find({ username: username });
-        res.status(200).send( { message: 'User fetched successfully!', data: user });
+        res.status(200).send( user);
     } catch (error) {
         console.log(error);
         res.status(400).send({ message: 'Failed to fetch user' });
@@ -84,7 +84,7 @@ app.get('/tasks/:username', async (req, res) => {
     const username = req.params.username;
     try {
         const tasks = await Task.find({ username: username});
-        res.status(200).send( { message: 'Tasks fetched successfully!', data: tasks });
+        res.status(200).send( tasks);
     } catch (error) {
         console.log(error);
         res.status(500).send({ message: 'Failed to fetch tasks' });
