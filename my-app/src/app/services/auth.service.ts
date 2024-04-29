2cace2e1803bbc63acae6cc97fc0dfa4b7b6ec14
Kelly Sun <kellysun0124@gmail.com>
2024-04-29 15:39:47 -0500

    added url to environemnts.ts and imported environment into auth.service.ts

--- my-app/src/app/services/auth.service.ts
+++ my-app/src/app/services/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
+import { environment } from '../../environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +14,7 @@ export class AuthService {
 
   login(username: string, password: string): Observable<any> {
     const userData = { userId: username, password: password };
-    return this.http.post<any>('http://localhost:3001/login', userData);
+    return this.http.post<any>(`${environment.apiUrl}/api/login`, userData);
   }
 
   saveLoggedInUser(user: any) {
--- my-app/src/environments/environment.ts
+++ my-app/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000',
+  apiUrl: 'http://http://44.223.33.61:4200/',
 };
 
