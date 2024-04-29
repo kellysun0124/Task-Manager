5fb05cbf37abdd6a447ab6eb241619c4d13c0bd9
Matthew Schutz <schutzmw3@gmail.com>
2024-04-29 15:23:48 -0500

    Adding example api service from assignment 4

--- my-app/src/app/api.service.ts
+++ my-app/src/app/api.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {BehaviorSubject, Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  private posts: Post[] = [];
+  private postsSubject: BehaviorSubject<Post[]> = new BehaviorSubject<Post[]>([]);
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getPosts(): Observable<Post[]> {
+    this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
+      .subscribe((postData) => {
+        this.posts = postData.posts;
+        this.postsSubject.next([...this.posts]);
+      })
+    return this.postsSubject.asObservable();
+  }
+
+  addPost(post: Post) {
+    this.posts = [...this.posts, post];
+    this.postsSubject.next(this.posts);
+  }
 }
