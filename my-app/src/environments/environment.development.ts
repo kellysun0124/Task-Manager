3f6a0e7811d12734a3ba5bf7adffa8da5736b965
Kelly Sun <kellysun0124@gmail.com>
2024-05-01 15:53:17 -0500

    change api url

--- my-app/src/environments/environment.development.ts
+++ my-app/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://54.91.122.204/'
-  //apiUrl: 'http://localhost:3000',
+  apiUrl: 'http://ec2-54-91-122-204.compute-1.amazonaws.com:3000/', // for server
+  //apiUrl: 'http://localhost:3000' for local
 
 };
