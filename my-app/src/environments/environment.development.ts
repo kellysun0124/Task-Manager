1bb3fa7e98ba66aa585ba92e29565d054a368cad
Matthew Schutz <schutzmw3@gmail.com>
2024-05-01 15:06:39 -0500

    Updating API url to new hostname

--- my-app/src/environments/environment.development.ts
+++ my-app/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://44.223.33.61:3000/', // for server
+  apiUrl: 'http://ec2-54-91-122-204.compute-1.amazonaws.com:3000/', // for server
   // apiUrl: 'http://localhost:3000/', // for local development
 };
